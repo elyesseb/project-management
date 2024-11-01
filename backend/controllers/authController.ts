c4bfa9a8660b963f6d3e784b8cd5d068262b99fb
elyesseb <elyesse.benhadj@gmail.com>
2024-11-01 21:29:03 +0100

    [ADD] login route with jwt added

--- backend/controllers/authController.ts
+++ backend/controllers/authController.ts
@@ -1,17 +1,19 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import { PrismaClient } from '@prisma/client';
+import jwt from 'jsonwebtoken';
 
 const prisma = new PrismaClient();
+const JWT_SECRET = process.env.JWT_SECRET || "secretKey";
 
 export const register = async (req: Request, res: Response) => {
     const { email, password } = req.body;
     try {
-        const existingUser = await prisma.user.findUnique({
+        const isUserExist = await prisma.user.findUnique({
             where: { email },
         });
 
-        if (existingUser) {
+        if (isUserExist) {
             return res.status(400).json({ message: 'Email already exists' });
         }
 
@@ -32,6 +34,33 @@ export const register = async (req: Request, res: Response) => {
         });
     } catch (error) {
         console.error('Error during registration', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+export const login = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: { email },
+        });
+
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+
+        const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: '1h' });
+
+        res.status(200).json({ message: 'Login successful', token });
+    } catch (error) {
+        console.error('Error during login', error);
         res.status(500).json({ message: 'Internal server error' });
     }
 };
--- backend/routes/authRoutes.ts
+++ backend/routes/authRoutes.ts
@@ -1,8 +1,9 @@
 import { Router, RequestHandler } from 'express';
-import { register } from '../controllers/authController';
+import { login, register } from '../controllers/authController';
 
 const router = Router();
 
 router.post('/register', register as RequestHandler);
+router.post('/login', login as RequestHandler);
 
 export default router;
